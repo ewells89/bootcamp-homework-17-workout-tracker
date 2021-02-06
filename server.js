3092a81aa5578984c0851f35f25995cd7bb84c3a
Elyse Wells <cewells89@gmail.com>
2021-02-06 00:49:32 -0500

    added server.js file. application launches successfully.

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require ("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false,
+    useCreateIndex:true 
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}.`);
+})
