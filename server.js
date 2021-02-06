1ba403d4962932bc7344d1150d44b2ba96d08b34
Elyse Wells <cewells89@gmail.com>
2021-02-06 02:12:52 -0500

    added html routes to server file. html routes working.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-
+const htmlRoutes = require("./routes/html-routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +13,7 @@ app.use(express.json());
 
 
 app.use(express.static("public"));
+app.use(htmlRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser:true,
