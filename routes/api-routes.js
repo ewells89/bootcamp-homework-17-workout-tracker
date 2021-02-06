36015a12f480408102f8192e292c8907d6d960d4
Elyse Wells <cewells89@gmail.com>
2021-02-06 02:45:41 -0500

    added api routes for adding/updating exercises and viewing dashboard stats

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,8 +20,56 @@ router.get("api/workouts", (req,res) => {
     });
 });
 
-// router.put()
+router.put("/api/workouts/:id", (req,res) => {
+    db.Workout.findByIdAndUpdate(
+        req.params.id,
+        {
+            $push: {
+                exercises: req.body,
+            },
+        },
+        {   
+            new: true,
+        },
+    )
+    .then(workout => {
+        res.json(workout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
-// router.post()
+router.post("/api/workouts", ({body},res) => {
+    db.Workout.create({})
+    .then (data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req,res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration" 
+                },
+            },
+        },
+    ])
+    .sort({
+        _id: -1
+    })
+    .limit (10)
+    .then(stats => {
+        res.json(stats)
+    })
+    .catch(err => {
+        res.json(err);
+    });
+})
 
 module.exports = router;
